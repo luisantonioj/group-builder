119f3693aed53b1cfa8337f1f483dccb01bbae68
luisantonioj <luisantoniojopia@gmail.com>
2026-05-18 20:11:34 +0800

    feat: add refresh in login to direct in dashboard after a successful login

--- group-builder-app/app/(auth)/login/page.tsx
+++ group-builder-app/app/(auth)/login/page.tsx
@@ -24,6 +24,7 @@ export default function LoginPage() {
     if (result?.error) {
       setError("Invalid email or password.");
     } else {
+      router.refresh();
       router.push("/dashboard");
     }
   }
